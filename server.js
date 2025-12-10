dcbbb66b9349d40d1206e4bac6c12a9255a99648
nicominue <nicominue@gmail.com>
2025-12-10 17:58:50 -0300

    fixing index.html

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use('/public', express.static(path.join(__dirname, 'public'), {
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 // In-memory game state (simple)
