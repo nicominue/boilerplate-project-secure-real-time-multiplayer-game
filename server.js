4a7e9598652d1f252091215bd9b84fcb5812b3c9
nicominue <nicominue@gmail.com>
2025-12-11 07:24:14 -0300

    testing changes

--- server.js
+++ server.js
@@ -11,10 +11,15 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use(helmet.noSniff());
-app.use(helmet.xssFilter());    
-app.use(helmet.noCache());
-app.use(helmet.hidePoweredBy());      
+app.use(helmet({
+  "X-Content-Type-Options": "noSniff",
+  "X-XSS-Protection": 0,
+  noCache : true,
+}));
+// app.use(helmet.noSniff());
+// app.use(helmet.xssFilter());    
+// app.use(helmet.noCache());
+// app.use(helmet.hidePoweredBy());      
 app.use((req, res, next) => {
   res.setHeader('X-Powered-By', 'PHP 7.4.3');
   next();
